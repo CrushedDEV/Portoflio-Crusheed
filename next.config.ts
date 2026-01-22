25fa769963b4bdbbed302d038cdb850ebeb3b07f
CrushedDev <rojito7676@gmail.com>
2026-01-22 11:16:43 +0100

    Update Next.js configuration to set basePath for production environment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/Portoflio-Crusheed" : "",
   images: {
     unoptimized: true,
   },
